a2ca8a8e54386091f6cec662785fedd5d14df2e7
Philip Li <philip.li@intel.com>
2022-11-06 22:39:43 +0800

    lib/common.rb: fix crash in MResultRootTable.open
    Traceback (most recent call last):
             8: from lib/nresult_root.rb:391:in `each'
             7: from lib/nresult_root.rb:345:in `mrt_table_set'
             6: from lib/nresult_root.rb:345:in `new'
             5: from lib/nresult_root.rb:278:in `initialize'
             4: from lib/nresult_root.rb:248:in `open'
             3: from lib/data_store.rb:835:in `open'
             2: from lib/common.rb:24:in `block in add_cached_method'
             1: from lib/common.rb:24:in `new'
    lib/nresult_root.rb:225:in `initialize': wrong number of arguments (given 2, expected 1) (ArgumentError)
    
    Fixes: c647829f9d7a ("lib/common.rb: fix lib/data_store.rb:62:in `initialize': wrong number of arguments (given 2, expected 1)")
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/common.rb
+++ lib/common.rb
@@ -21,7 +21,11 @@ module AddCachedMethod
       @__cache_for_add_cached_method__ ||= {}
       cache = @__cache_for_add_cached_method__
       mkey = [method_name, key]
-      cache[mkey] ||= send(method_name, *args, **kwargs)
+      cache[mkey] ||= if kwargs.empty?
+                        send(method_name, *args)
+                      else
+                        send(method_name, *args, **kwargs)
+                      end
     end
   end
 end

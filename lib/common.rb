weishan18/lkp-tests

commit c647829f9d7a578c9c31bf735d9c1172e437cf12

lib/common.rb: fix lib/data_store.rb:62:in `initialize': wrong number of arguments (given 2, expected 1)
This occurs on ruby 2.7 and above

/home/lkp-tests/lib/data_store.rb:62:in `initialize': wrong number of arguments (given 2, expected 1) (ArgumentError)
        from /home/lkp-tests/lib/common.rb:24:in `new'
        from /home/lkp-tests/lib/common.rb:24:in `block in add_cached_method'
        from /home/lkp-tests/lib/data_store.rb:246:in `create_new'
        from /home/lkp-tests/lib/nresult_root.rb:241:in `create_layout'
        from /home/lkp-tests/lib/nresult_root.rb:337:in `create_tables_layout'
        from /home/lkp-tests/bin/run-local:168:in `<main>'

Link: https://www.ruby-lang.org/en/news/2019/12/12/separation-of-positional-and-keyword-arguments-in-ruby-3-0/
Link: https://github.com/intel/lkp-tests/issues/185
Signed-off-by: Philip Li <philip.li@intel.com>

lib/common.rb
@@ -17,11 +17,11 @@ end
 # Take a look at class Commit for usage
 module AddCachedMethod
   def add_cached_method(method_name, prefix = 'cached_')
-    define_method("#{prefix}#{method_name}") do |key, *args|
+    define_method("#{prefix}#{method_name}") do |key, *args, **kwargs|
       @__cache_for_add_cached_method__ ||= {}
       cache = @__cache_for_add_cached_method__
       mkey = [method_name, key]
-      cache[mkey] ||= send(method_name, *args)
+      cache[mkey] ||= send(method_name, *args, **kwargs)
     end
   end
 end

commit a2ca8a8e54386091f6cec662785fedd5d14df2e7

lib/common.rb: fix crash in MResultRootTable.open
Traceback (most recent call last):
         8: from lib/nresult_root.rb:391:in `each'
         7: from lib/nresult_root.rb:345:in `mrt_table_set'
         6: from lib/nresult_root.rb:345:in `new'
         5: from lib/nresult_root.rb:278:in `initialize'
         4: from lib/nresult_root.rb:248:in `open'
         3: from lib/data_store.rb:835:in `open'
         2: from lib/common.rb:24:in `block in add_cached_method'
         1: from lib/common.rb:24:in `new'
lib/nresult_root.rb:225:in `initialize': wrong number of arguments (given 2, expected 1) (ArgumentError)

Fixes: c647829f9d7a ("lib/common.rb: fix lib/data_store.rb:62:in `initialize': wrong number of arguments (given 2, expected 1)")
Signed-off-by: Philip Li <philip.li@intel.com>

lib/common.rb
@@ -21,7 +21,11 @@ module AddCachedMethod
       @__cache_for_add_cached_method__ ||= {}
       cache = @__cache_for_add_cached_method__
       mkey = [method_name, key]
-      cache[mkey] ||= send(method_name, *args, **kwargs)
+      cache[mkey] ||= if kwargs.empty?
+                        send(method_name, *args)
+                      else
+                        send(method_name, *args, **kwargs)
+                      end
     end
   end
 end