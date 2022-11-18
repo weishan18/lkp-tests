df2d69b33d745f66be80b11ba92f0a499bc2dc7f
Philip Li <philip.li@intel.com>
2022-11-18 12:04:50 +0800

    Revert "lib/bootstrap.sh: change load_disk_fail to load_disk.fail"
    This reverts commit 2c9b841998b494dec0c0cad78e0539d1db3ca137.
    
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/failure_spec.rb
+++ spec/failure_spec.rb
@@ -3,7 +3,7 @@ require "#{LKP_SRC}/lib/stats"
 
 expects = {
   'dmesg.INFO:task_blocked_for_more_than#seconds' => true,
-  'last_state.running' => true,
+  'last_state.load_disk_fail' => true,
   'xfstests.btrfs.192.fail' => true,
   'perf-profile.calltrace.cycles-pp.error_entry' => false
 }
