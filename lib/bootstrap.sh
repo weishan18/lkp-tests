6261cd91f5940c1eaaa4971e24d87f3324f33d9d
Philip Li <philip.li@intel.com>
2022-11-18 11:59:16 +0800

    Revert "lib/bootstrap.sh: continue testing when fail to load rootfs partition"
    This reverts commit de937c3d00b03b20a31bba2855d8fb807b7a3be7.
    
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -413,29 +413,21 @@ mount_rootfs()
 {
 	if [ -n "$rootfs_partition" ]; then
 		# wait for the machine to load the disk
-		if wait_load_disk; then
-			set_job_state 'load_disk.pass'
-
-			ROOTFS_DIR=/opt/rootfs
-			mkdir -p $ROOTFS_DIR
-
-			mount $rootfs_partition $ROOTFS_DIR || {
-				mkfs.btrfs -f $rootfs_partition
-				mount $rootfs_partition $ROOTFS_DIR
-			}
-
-			mkdir -p $ROOTFS_DIR/tmp
-			CACHE_DIR=$ROOTFS_DIR/tmp
-			cleanup_pkg_cache $CACHE_DIR
-		else
-			# rli9 FIXME: continue the test unless the test requires to use rootfs
-			# on the rootfs_partition
-			echo "can't load the disk $rootfs_partition, try to continue the testing..."
-			set_job_state 'load_disk.fail'
-
-			CACHE_DIR=/tmp/cache
-			mkdir -p $CACHE_DIR
-		fi
+		wait_load_disk || {
+			# skipping following test if disk can't be load
+			echo "can't load the disk $rootfs_partition, skip testing..."
+			set_job_state 'load_disk_fail'
+			return 1
+		}
+		ROOTFS_DIR=/opt/rootfs
+		mkdir -p $ROOTFS_DIR
+		mount $rootfs_partition $ROOTFS_DIR || {
+			mkfs.btrfs -f $rootfs_partition
+			mount $rootfs_partition $ROOTFS_DIR
+		}
+		mkdir -p $ROOTFS_DIR/tmp
+		CACHE_DIR=$ROOTFS_DIR/tmp
+		cleanup_pkg_cache $CACHE_DIR
 	else
 		CACHE_DIR=/tmp/cache
 		mkdir -p $CACHE_DIR
