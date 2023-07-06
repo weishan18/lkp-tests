weishan18/lkp-tests

commit fb2f4a3c4b1485b87c0369a9ad5d68323ce7ee89

lib/kexec.sh: fix acpi rsdp pattern to support hex
Fixes: 03eb059dc46a ("lib/kexec.sh: read acpi_rsdp from dmesg in noefi case")
Signed-off-by: Yujie Liu <yujie.liu@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/kexec.sh
@@ -192,8 +192,9 @@ kexec_to_next_job()
 	# if dmesg has "ACPI: RSDP 0x0000...", it must be a correct value of a successful boot
 	# if dmesg has "ACPI:      0x0000..." (missing RSDP keyword), it means the value is a wrong one
 	# [    0.008415] ACPI: RSDP 0x0000000036937000 000024 (v02 ALASKA)
+	# [    1.212964] ACPI: RSDP 0x00000000699FD014 000024 (v02 INTEL )
 	[ -n "$acpi_rsdp" ] || {
-		local dmesg_acpi_rsdp=$(dmesg | grep -m1 "ACPI: RSDP" | grep -o -E "0x[0-9]+")
+		local dmesg_acpi_rsdp=$(dmesg | grep -m1 "ACPI: RSDP" | grep -o -E "0x[0-9a-zA-Z]+")
 		if [ -n "$dmesg_acpi_rsdp" ]; then
 			# get the last 8 digits
 			acpi_rsdp="0x$(echo $dmesg_acpi_rsdp | cut -c 11-)"

commit 5c2164a7c3eb597a30e89089b6c930ac9b404c79

lib/kexec.sh: match hex value accurately
Fixes: fb2f4a3c4b14 ("lib/kexec.sh: fix acpi rsdp pattern to support hex")
Signed-off-by: Yujie Liu <yujie.liu@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/kexec.sh
@@ -194,7 +194,7 @@ kexec_to_next_job()
 	# [    0.008415] ACPI: RSDP 0x0000000036937000 000024 (v02 ALASKA)
 	# [    1.212964] ACPI: RSDP 0x00000000699FD014 000024 (v02 INTEL )
 	[ -n "$acpi_rsdp" ] || {
-		local dmesg_acpi_rsdp=$(dmesg | grep -m1 "ACPI: RSDP" | grep -o -E "0x[0-9a-zA-Z]+")
+		local dmesg_acpi_rsdp=$(dmesg | grep -m1 "ACPI: RSDP" | grep -o -E "0x[0-9a-fA-F]+")
 		if [ -n "$dmesg_acpi_rsdp" ]; then
 			# get the last 8 digits
 			acpi_rsdp="0x$(echo $dmesg_acpi_rsdp | cut -c 11-)"