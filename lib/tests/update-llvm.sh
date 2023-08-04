ac8afef3b93e4a6da4ebfab40270513c71f0d48a
Yujie Liu <yujie.liu@intel.com>
2023-08-04 16:09:16 +0800

    lib/tests/update-llvm.sh: fix wrong truncation of llmv version
    a long lasting bug that has never been noticed since 2020...
    
    Reported-by: Wu Zheng <wu.zheng@intel.com>
    Cc: Wu Zheng <wu.zheng@intel.com>
    Fixes: 76427ec98738 ("tests/kernel_selftests.sh: check llvm for bpf")
    Signed-off-by: Yujie Liu <yujie.liu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/update-llvm.sh
+++ lib/tests/update-llvm.sh
@@ -5,9 +5,12 @@
 # mannually.
 prepare_for_llvm()
 {
+	#   LLVM version 11.0.1
+	# Ubuntu LLVM version 14.0.0
 	local llvm_version=$(llc --version | grep "LLVM version")
-	llvm_version=${llvm_version## *}
+	llvm_version=${llvm_version##* }
 	llvm_version=${llvm_version%%.*}
+	echo "llvm_version: $llvm_version"
 	[[ $llvm_version -lt 10 ]] && {
 		echo "Please install llvm-10 before running bpf"
 		return 1
