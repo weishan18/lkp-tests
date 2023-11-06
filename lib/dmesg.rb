weishan18/lkp-tests

commit a28b380ee40f4014323f6a1a6e7910d33e22ed32

lib/dmesg.rb: fix "dmesg contains source prefix" sometimes
some decode_stacktrace doesn't contain DECODE_FLAG

fix the dmesg which still showed source code prefix
--------------
[ 292.363140 ][ T1 ] dump_stack (kbuild/src/consumer/lib/dump_stack.c:131)
[ 292.363551 ][ T1 ] dump_header (kbuild/src/consumer/mm/oom_kill.c:463)
[ 292.363969 ][ T1 ] ? out_of_memory (kbuild/src/consumer/include/linux/rcupdate.h:710 kbuild/src/consumer/mm/oom_kill.c:379 kbuild/src/consumer/mm/oom_kill.c:1102 kbuild/src/consumer/mm/oom_kill.c:1048)
[ 292.364439 ][ T1 ] out_of_memory.cold (kbuild/src/consumer/mm/oom_kill.c:1106 kbuild/src/consumer/mm/oom_kill.c:1048)
[ 292.365422 ][ T1 ] __alloc_pages_slowpath+0x73e/0xbc0
--------------

Link: https://lore.kernel.org/lkml/20210729014019.GA22801@xsang-OptiPlex-9020/
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/dmesg.rb
@@ -490,7 +490,7 @@ def get_crash_calltraces(dmesg_file)
   in_decode = false
   end_decode = false
   decode_stacktrace = dmesg_content.include?(DECODE_FLAG)
-  dmesg_content.gsub!('kbuild/src/consumer/', '') if decode_stacktrace
+  dmesg_content.gsub!('kbuild/src/consumer/', '') if dmesg_content.include?('kbuild/src/consumer/')
 
   dmesg_content.each_line do |line|
     if line =~ / BUG: | WARNING: | INFO: | UBSAN: | kernel BUG at /

commit df2c035ba300316aa0425b6618e62020d778a366

lib/dmesg.rb: remove more worker prefixes
we used to consider the case of consumer worker, but it can also be
other workers, so there are still source prefixes in the report that is
useless to users and also makes the line very long. fix this by removing
any possible prefixes.

[ 292.363140 ][ T1 ] dump_stack (kbuild/src/consumer/lib/dump_stack.c:131)
[ 7.924990][ T6] ? page_fault_oops (kbuild/src/rand-x86_64-2/arch/x86/mm/fault.c:707)

Fixes: a28b380ee40f ("lib/dmesg.rb: fix "dmesg contains source prefix" sometimes")
Fixes: 51e31c65c31d ("lib/dmesg.rb: don't print kernel source path in get_crash_calltraces")
Signed-off-by: Yujie Liu <yujie.liu@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/dmesg.rb
@@ -439,7 +439,7 @@ def get_crash_calltraces(dmesg_file)
   in_decode = false
   end_decode = false
   decode_stacktrace = dmesg_content.include?(DECODE_FLAG)
-  dmesg_content.gsub!('kbuild/src/consumer/', '') if dmesg_content.include?('kbuild/src/consumer/')
+  dmesg_content.gsub!(/kbuild\/src\/[^\/]+\//, '')
 
   dmesg_content.each_line do |line|
     if line =~ /---\[ cut here | BUG: | WARNING: | INFO: | UBSAN: | kernel BUG at /