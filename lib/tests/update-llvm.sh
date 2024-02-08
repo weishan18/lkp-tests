291c5afd87f9c85cd43e8922e0c614330a137cc4
Yujie Liu <yujie.liu@intel.com>
2024-02-08 13:57:49 +0800

    lib/tests/update-llvm.sh: update minimum clang version of building bpf selftest
    > BTW, I failed to compile the latest selftests/bpf with
    > the following errors:
    >
    > progs/verifier_and.c:58:16: error: invalid operand for instruction
    >         asm volatile ("                                 \
    >
    
    These tests were moved to use inline assembly recently (2 month ago).
    Discussion at the time was whether to use \n\ or \ terminators at the
    end of each line. People opted for \ as easier to read.
    Replacing \ with \n\ and compiling this test using clang 14 shows
    more informative error message:
    
    $ make -j14 `pwd`/verifier_and.bpf.o
      CLNG-BPF [test_maps] verifier_and.bpf.o
    progs/verifier_and.c:68:1: error: invalid operand for instruction
            w1 %%= 2;                                       \n\
    ^
    <inline asm>:11:5: note: instantiated into assembly here
            w1 %= 2;
    
    My guess is that clang 14 does not know how to handle operations on
    32-bit sub-registers w[0-9].
    
    But using clang 14 I get some errors not related to inline assembly as well.
    Also, I recall that there were runtime issues with clang 14 and
    tests using enum64.
    
    All-in-all, you need newer version of clang for tests nowadays,
    sorry for inconvenience.
    
    Link: https://lore.kernel.org/netdev/34fb3a9841bf4977413be799f7cbef78560aaa20.camel@gmail.com/
    Signed-off-by: Yujie Liu <yujie.liu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/update-llvm.sh
+++ lib/tests/update-llvm.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# As of now, make bpf failed when we use clang under v10.
-# prepare_for_llvm works for cluster, for local user, please install clang v10
+. $LKP_SRC/lib/env.sh
+
+# make bpf failed when we use clang under v15 on v6.7 kernel
+# prepare_for_llvm works for cluster, for local user, please install clang v15 or newer
 # mannually.
 prepare_for_llvm()
 {
 	# Due to some low dependency version issues, the latest version of the llvm_project.cgz package
 	# cannot be generated in alios.
-	# But bpf must use llvm v10 or higher, so don't run bpf on alios.
+	# But bpf must use llvm v15 or higher, so don't run bpf on alios.
 	is_aliyunos && die "alios doesn't support bpf due to some dependency issues"
 
 	#   LLVM version 11.0.1
@@ -16,8 +18,8 @@ prepare_for_llvm()
 	llvm_version=${llvm_version##* }
 	llvm_version=${llvm_version%%.*}
 	echo "llvm_version: $llvm_version"
-	[[ $llvm_version -ge 10 ]] || {
-		echo "Please install llvm-10 or newer before running bpf"
+	[[ $llvm_version -ge 15 ]] || {
+		echo "Please install llvm-15 or newer before running bpf"
 		return 1
 	}
 }
