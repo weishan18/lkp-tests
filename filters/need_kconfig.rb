weishan18/lkp-tests

commit 7c5636de0faddd9d80da03f8f336550ca016a328

filters/need_kconfig.rb: refactor to extract a few methods
Signed-off-by: Philip Li <philip.li@intel.com>

filters/need_kconfig.rb
@@ -12,7 +12,11 @@ def load_kernel_context
   context_file = File.expand_path '../context.yaml', kernel
   raise Job::ParamError, "context.yaml doesn't exist: #{context_file}" unless File.exist?(context_file)
 
-  YAML.load(File.read(context_file))
+  context = OpenStruct.new YAML.load(File.read(context_file))
+  context.kernel_version = context['rc_tag']
+  context.kernel_arch = context['kconfig'].split('-').first
+
+  context
 end
 
 def read_kernel_kconfigs
@@ -78,44 +82,44 @@ def split_constraints(constraints)
   OpenStruct.new(kernel_versions: kernel_versions, archs: archs, types: types)
 end
 
-def check_all(kernel_kconfigs, needed_kconfigs)
-  uncompiled_kconfigs = []
-
-  context = load_kernel_context
-  kernel_version = context['rc_tag']
-  kernel_arch = context['kconfig'].split('-').first
-
+def load_kconfig_constraints
   kconfigs_yaml = KernelTag.kconfigs_yaml
-  if File.size? kconfigs_yaml
-    kconfig_constraints = YAML.load_file kconfigs_yaml
-    kconfig_constraints = kconfig_constraints.transform_values { |constraints| split_constraints(constraints) }
+  return {} unless File.size? kconfigs_yaml
+
+  YAML.load_file(kconfigs_yaml)
+      .transform_values { |constraints| split_constraints(constraints) }
+end
+
+def parse_needed_kconfig(e)
+  if e.instance_of? Hashugar
+    # e.to_hash: {"KVM_GUEST"=>"y"}
+    e.to_hash.first
   else
-    kconfig_constraints = {}
+    # e: IA32_EMULATION=y
+    # e: SATA_AHCI
+    e.split('=')
   end
+end
 
-  needed_kconfigs.each do |e|
-    config_name, config_options = if e.instance_of? Hashugar
-                                    # e.to_hash: {"KVM_GUEST"=>"y"}
-                                    e.to_hash.first
-                                  else
-                                    # e: IA32_EMULATION=y
-                                    # e: SATA_AHCI
-                                    e.split('=')
-                                  end
+def check_all(kernel_kconfigs, needed_kconfigs)
+  context = load_kernel_context
+  kconfig_constraints = load_kconfig_constraints
 
+  uncompiled_kconfigs = needed_kconfigs.map do |e|
+    config_name, config_options = parse_needed_kconfig(e)
     config_options = split_constraints(config_options)
 
     # global arch constraint that means the kconfig is only supported on the specific arch
     expected_archs = kconfig_constraints[config_name].archs if kconfig_constraints[config_name]
-    next unless expected_archs.nil? || expected_archs.empty? || kernel_match_arch?(kernel_arch, expected_archs)
+    next unless expected_archs.nil? || expected_archs.empty? || kernel_match_arch?(context.kernel_arch, expected_archs)
 
     # test arch constraint that means the kconfig is only enabled on the specific arch as required by test
-    next unless config_options.archs.empty? || kernel_match_arch?(kernel_arch, config_options.archs)
+    next unless config_options.archs.empty? || kernel_match_arch?(context.kernel_arch, config_options.archs)
 
     if kconfig_constraints[config_name]
       expected_kernel_versions = kconfig_constraints[config_name].kernel_versions
       # ignore the check of kconfig type if kernel is not within the valid range
-      next if expected_kernel_versions && !kernel_match_version?(kernel_version, expected_kernel_versions)
+      next if expected_kernel_versions && !kernel_match_version?(context.kernel_version, expected_kernel_versions)
     end
 
     expected_kernel_kconfig = "#{config_name}=#{config_options.types.first}"
@@ -124,14 +128,13 @@ def check_all(kernel_kconfigs, needed_kconfigs)
 
     uncompiled_kconfig = expected_kernel_kconfig
     uncompiled_kconfig += " (#{expected_kernel_versions.join(', ').delete('"')})" if expected_kernel_versions
-    uncompiled_kconfigs.push uncompiled_kconfig
+    uncompiled_kconfig
   end
 
-  return nil if uncompiled_kconfigs.empty?
+  uncompiled_kconfigs = uncompiled_kconfigs.compact.sort.uniq
+  return if uncompiled_kconfigs.empty?
 
-  uncompiled_kconfigs = uncompiled_kconfigs.sort.uniq
-
-  kconfigs_error_message = "#{File.basename __FILE__}: #{uncompiled_kconfigs} has not been compiled by this kernel (#{kernel_version} based) per #{kconfigs_yaml}"
+  kconfigs_error_message = "#{File.basename __FILE__}: #{uncompiled_kconfigs} has not been compiled by this kernel (#{context.kernel_version} based) per #{kconfigs_yaml}"
   raise Job::ParamError, kconfigs_error_message.to_s unless __FILE__ =~ /suggest_kconfig/
 
   puts "suggest kconfigs: #{uncompiled_kconfigs}"

commit 88d2b8d12dff29cf160e59eb63258192126ac200

filters/need_kconfig.rb: fix undefined kconfigs_yaml
Fixes: 7c5636de0fad ("filters/need_kconfig.rb: refactor to extract a few methods")
Signed-off-by: Philip Li <philip.li@intel.com>

filters/need_kconfig.rb
@@ -82,8 +82,7 @@ def split_constraints(constraints)
   OpenStruct.new(kernel_versions: kernel_versions, archs: archs, types: types)
 end
 
-def load_kconfig_constraints
-  kconfigs_yaml = KernelTag.kconfigs_yaml
+def load_kconfig_constraints(kconfigs_yaml)
   return {} unless File.size? kconfigs_yaml
 
   YAML.load_file(kconfigs_yaml)
@@ -103,7 +102,9 @@ end
 
 def check_all(kernel_kconfigs, needed_kconfigs)
   context = load_kernel_context
-  kconfig_constraints = load_kconfig_constraints
+
+  kconfigs_yaml = KernelTag.kconfigs_yaml
+  kconfig_constraints = load_kconfig_constraints(kconfigs_yaml)
 
   uncompiled_kconfigs = needed_kconfigs.map do |e|
     config_name, config_options = parse_needed_kconfig(e)